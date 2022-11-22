a281e774089744ea39329ce38f9929da9b46ace7
Adriana sm <a.s.m27267@gmail.com>
2022-11-22 20:57:56 -0300

    mongoose

---
+++ Config/db.js
@@ -0,0 +1,5 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb+srv://adriana:<password>@svl-db.7j0jdwb.mongodb.net/');
+
+module.exports = mongoose
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# svl-api
+
+# Configurando o banco(Mongo DB)
+## Criar o arquivo de conexão usando o mongoose
+/config
+
+## Clone do projeto 
+--git clone
