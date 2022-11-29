47d2d879eeab1194dfd5fac9658bd25901babcfc
Adriana sm <a.s.m27267@gmail.com>
2022-11-29 20:58:28 -0300

    configurando o db

--- server.js
+++ server.js
@@ -1,5 +1,12 @@
 const express = require("express");
+const db = require ('./config/db')
 const app = express();
+
+db.on('error', console.log.bind('Erro ao conectar com o banco!'))
+db.once('open',() =>{
+console.log('Conexão realizada com sucesso!!')
+})
+
 const port = 5000;
 
 app.get("/", (req, res) => {
