585813a690b81f9b92ad13d7b9621bfe857910ba
Adriana sm <a.s.m27267@gmail.com>
2022-12-01 20:50:34 -0300

    get livros

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const db = require ('./config/db')
 const app = express();
+const livros = require ('./models/Livros')
 
 db.on('error', console.log.bind('Erro ao conectar com o banco!'))
 db.once('open',() =>{
@@ -16,3 +17,13 @@ app.get("/", (req, res) => {
 app.listen(port, () => {
   console.log(`Servidor rodando ${port}`);
 });
+
+
+app.get('/livros', (req,res)=>{
+livros.find((error, livros)=>{
+  res.status(200).json(livros)
+})
+})
+//app.get('/livros/id',(req,res)=>{
+//findById()
+//})
