37d01d2c6e592e76815e67402f10b8ec44c67509
Adrianasmagalhaes <105891851+Adrianasmagalhaes@users.noreply.github.com>
2022-12-14 11:33:30 -0300

    Update LivrosControllers.js

--- controllers/LivrosControllers.js
+++ controllers/LivrosControllers.js
@@ -1,19 +1,45 @@
-const livros = require('../models/Livros')
+module.exports=LivroController
 
+const livros = require('../models/livro')
 
 class LivroController {
-    static findAll = (req,res)=>{
-        livros.find((err, livros)=>{
+
+    static findAll = (req, res) => {
+        livros.find((err, livros) => {
+            console.log('find', livros)
             res.status(200).json(livros)
         })
     }
-    static findById = (req,res)=>{
+
+    static getBookById = (req, res) => {
         const id = req.params.id
-        livros.findById((err, livros)=>{
-            
-            res.status(200).json(livros)
+
+        livros.findById(id, (err, livros) => {
+
+            //tratar o erro ou o sucesso , baseado na função de callback utilizando os parametros err, livros
+            if (err) {
+                console.log('1')
+                res.status(404).send({ message: 'Livro não encontrado', erro: err.message })
+            } else {
+                console.log('2')
+                res.status(200).json(livros)
+            }
         })
-    } 
+    }
+
+    static createBook = (req, res) => {
+        let livro = new livros(req.body)
+
+        console.log('save', livro)
+
+        livro.save(err => {
+            if (err) {
+                res.status(500).send({ message: 'Error ao salvar o livro', erro: err.message })
+            } else {
+                res.status(201).send(livro.toJSON())
+            }
+        })
+    }
 }
 
-module.exports=LivroController
+module.exports = LivroController
