9416d7e087167fb756d5fcb9e02c1ace4f377b50
Adriana sm <a.s.m27267@gmail.com>
2022-12-15 21:03:21 -0300

    métodos, Post, Delete,

--- controllers/LivrosControllers.js
+++ controllers/LivrosControllers.js
@@ -1,45 +1,57 @@
-module.exports=LivroController
-
-const livros = require('../models/livro')
+const livros = require("../models/Livros");
 
 class LivroController {
+  static findAll = (req, res) => {
+    livros.find((err, livros) => {
+      console.log("find", livros);
+      res.status(200).json(livros);
+    });
+  };
 
-    static findAll = (req, res) => {
-        livros.find((err, livros) => {
-            console.log('find', livros)
-            res.status(200).json(livros)
-        })
-    }
+  static getBookById = (req, res) => {
+    const id = req.params.id;
 
-    static getBookById = (req, res) => {
-        const id = req.params.id
+    livros.findById(id, (err, livros) => {
+      //tratar o erro ou o sucesso , baseado na função de callback utilizando os parametros err, livros
+      if (err) {
+        console.log("1");
+        res
+          .status(404)
+          .send({ message: "Livro não encontrado", erro: err.message });
+      } else {
+        console.log("2");
+        res.status(200).json(livros);
+      }
+    });
+  };
 
-        livros.findById(id, (err, livros) => {
+  static createBook = (req, res) => {
+    let livro = new livros(req.body);
 
-            //tratar o erro ou o sucesso , baseado na função de callback utilizando os parametros err, livros
-            if (err) {
-                console.log('1')
-                res.status(404).send({ message: 'Livro não encontrado', erro: err.message })
-            } else {
-                console.log('2')
-                res.status(200).json(livros)
-            }
-        })
-    }
+    console.log("save", livro);
 
-    static createBook = (req, res) => {
-        let livro = new livros(req.body)
+    livro.save((err) => {
+      if (err) {
+        res
+          .status(500)
+          .send({ message: "Error ao salvar o livro", erro: err.message });
+      } else {
+        res.status(201).send(livro.toJSON());
+      }
+    });
+  };
+  static deleteBook = (req,res)=>{
+    const id = req.params.id
 
-        console.log('save', livro)
-
-        livro.save(err => {
-            if (err) {
-                res.status(500).send({ message: 'Error ao salvar o livro', erro: err.message })
-            } else {
-                res.status(201).send(livro.toJSON())
-            }
-        })
-    }
+    livros.findByIdAndDelete( id, (err)=>{
+        if(err){
+            res
+          .status(404)
+          .send({ message: "Não foi possivel deletar!Livro não encontrado", erro: err.message })
+        }else{;
+        res.status(200).json("Deletado");}
+    })
+  }
 }
 
-module.exports = LivroController
+module.exports = LivroController;
--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,4 @@
-const express=require('express')
+const express= require('express')
 const livros = require ('./livrosRoutes')
 
 const routes=(app)=>{
